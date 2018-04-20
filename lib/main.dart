063af54b78a0bbf7c6e0513f4e9e67dd887210a4
kuronekomichael <hardwarehacking@gmail.com>
2018-04-20 13:27:54 +0900

    Saved at 13:00

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 //void main() => runApp(new MyApp());
 void main() => runApp(new TabBarDemo());
 
-class TabBarDemo extends StatelessWidget {
+class TabBarDemoState extends State {
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
@@ -12,6 +13,10 @@ class TabBarDemo extends StatelessWidget {
         child: new Scaffold(
           appBar: new AppBar(
             title: new Text('Todo MVC'),
+            actions: <Widget>[
+              new IconButton(icon: new Icon(Icons.airport_shuttle), ),
+              new IconButton(icon: new Icon(Icons.screen_lock_landscape), ),
+            ],
           ),
           body: new TabBarView(
             children: [
@@ -19,6 +24,7 @@ class TabBarDemo extends StatelessWidget {
               new Icon(Icons.directions_transit),
             ],
           ),
+          // TODO BarItemに境界線ほしいよね
           bottomNavigationBar: new BottomNavigationBar(currentIndex: 0, items: [
             new BottomNavigationBarItem(
               icon: new Icon(Icons.featured_play_list),
@@ -28,115 +34,24 @@ class TabBarDemo extends StatelessWidget {
               icon: new Icon(Icons.show_chart),
               title: new Text("Stats"),
             ),
+
           ]),
+          floatingActionButton: new FloatingActionButton(
+            tooltip: 'Increment',
+            child: new Icon(Icons.add),
+            onPressed: _pushSaved,
+          ), // This trailing comma makes auto-formatting nicer for build methods.
         ),
       ),
     );
   }
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'Todo MVC'),
-    );
+  void _pushSaved() {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class TabBarDemo extends StatefulWidget {
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  State createState(){
+    return new TabBarDemoState();
   }
 }
